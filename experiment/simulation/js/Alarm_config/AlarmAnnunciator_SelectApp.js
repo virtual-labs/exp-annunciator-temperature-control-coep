605d9732cfd37e9915cd5a259c6e0e906dcca094
vaibhavkurhe27 <vrk.vlab@coep.ac.in>
2021-01-21 18:01:33 +0530

    Exp updated

--- experiment/simulation/js/Alarm_config/AlarmAnnunciator_SelectApp.js
+++ experiment/simulation/js/Alarm_config/AlarmAnnunciator_SelectApp.js
@@ -14,10 +14,10 @@ AlarmAnnunciator_SelectApp = function(){
 		    +'<label>Select Application:</label>'
 		    +'<select class="form-control" name="App" id="SelectAppID" >'
 			+'<option value="-1">Select</option>'
-			+'<option value="1">Level Control System</option>'
+			//+'<option value="1">Level Control System</option>'
 			+'<option value="2">Temperature Control System</option>'
-			+'<option value="3">Pressure Control System</option>'
-			+'<option value="4">Flow Control System</option>'
+			//+'<option value="3">Pressure Control System</option>'
+			//+'<option value="4">Flow Control System</option>'
 			+'</select>'
 		    
 				+'</div>' // col close
